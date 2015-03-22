b91d2a79683b7861f481497f48bef885eca446e2
Steven A. Cholewiak <scholewiak@gmail.com>
2015-03-22 21:04:44 +0100

    Made Python 2 and 3 compatible with modernize

--- enforceflairbot.py
+++ enforceflairbot.py
@@ -1,19 +1,22 @@
-from ConfigParser import SafeConfigParser
-import cPickle
+from __future__ import absolute_import
+from __future__ import print_function
+from six.moves.configparser import SafeConfigParser
+import six.moves.cPickle
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import praw
 import os
 import time
+import six
 
 def save_hist(data, path):
     with open(path, 'wb') as f:
-        cPickle.dump(data, f)
+        six.moves.cPickle.dump(data, f)
         
 def load_hist(path):
     if os.path.isfile(path):
         with open(path, 'rb') as f:
-            return cPickle.load(f)
+            return six.moves.cPickle.load(f)
     else:
         return {}
         
@@ -25,15 +28,15 @@ reddit_username = config.get('reddit', 'username')
 reddit_password = config.get('reddit', 'password')
 reddit_user_agent = config.get('reddit', 'user_agent')
 
-print 'Loading history'
+print('Loading history')
 tracked_submissions = load_hist('enforceflairbot_history.p')
 
-print 'Logging into Reddit'
+print('Logging into Reddit')
 r = praw.Reddit(user_agent=reddit_user_agent)
 r.login(reddit_username, reddit_password)
 
 while True:
-    print 'Running bot: {0}'.format(datetime.utcnow())
+    print('Running bot: {0}'.format(datetime.utcnow()))
     # Add new submissions
     subreddit = r.get_subreddit(config.get('reddit', 'subreddit'))
     for submission in subreddit.get_new(limit=10):
@@ -50,7 +53,7 @@ while True:
             
     save_hist(tracked_submissions, 'enforceflairbot_history.p')
 
-    for submission_id, submission in tracked_submissions.iteritems():
+    for submission_id, submission in six.iteritems(tracked_submissions):
         if submission['link_flair_text']:
             # Submission had link flair, continue to the next
             continue
@@ -67,12 +70,12 @@ while True:
         if not submission['link_flair_text']:
             
             if datetime.utcnow() > (submission['created_utc'] + relativedelta(minutes=5)) and not submission['messaged']:
-                print 'Over 5 minutes have passed, message user for submission {0}'.format(submission['short_link'])
+                print('Over 5 minutes have passed, message user for submission {0}'.format(submission['short_link']))
                 r.send_message(recipient=s.author, subject='Add link flair within 10 minutes or submission will be removed', message='Please add subreddit-approved link flair to [this post]({0}), otherwise it will be automatically removed in 10 minutes.'.format(submission['short_link']))
                 submission['messaged'] = datetime.utcnow()
                     
             if submission['messaged'] and datetime.utcnow() > (submission['messaged'] + relativedelta(minutes=10)):
-                print '10 minutes have passed since message, delete submission {0}'.format(submission['short_link'])
+                print('10 minutes have passed since message, delete submission {0}'.format(submission['short_link']))
                 s.add_comment('This submission has been removed because it was not appropriately flaired.  If you feel that this removal was in error, please message the moderators.')
                 s.remove(spam=False)
                 submission['deleted'] = datetime.utcnow()
